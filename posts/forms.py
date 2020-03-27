097c7192675c9ba53e5d5e04fc67cdf632e33bf6
SAIFEDDIN Matoui <saifeddinmatoui4@gmail.com>
2020-03-27 16:34:02 +0100

    Automated creation and update date

--- posts/forms.py
+++ posts/forms.py
@@ -5,15 +5,19 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ['title', 'content','created_at']
-        widgets = {
-            'created_at' : forms.DateInput(
-                attrs={
-                'type': 'date',
-                'data-provide': 'datepicker',
-                'data-date-format': 'yyyy-mm-dd',
-            },
-            format='%Y-%m-%d'
-                    )
+        fields = ['title', 'content']
 
-        }
+
+
+        
+        # widgets = {
+        #     'created_at' : forms.DateInput(
+        #         attrs={
+        #         'type': 'date',
+        #         'data-provide': 'datepicker',
+        #         'data-date-format': 'yyyy-mm-dd',
+        #     },
+        #     format='%Y-%m-%d'
+        #             )
+
+        # }
--- posts/models.py
+++ posts/models.py
@@ -1,17 +1,25 @@
 from django.db import models
 from django.conf import settings
+from django.utils import timezone
 
 class Post(models.Model):
     user       = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE)
     title      = models.CharField(max_length=120)                  
     content      = models.TextField()
-    created_at   = models.DateField(auto_now=False, auto_now_add=False)
+    created_at   = models.DateField(auto_now=False, auto_now_add=False, null=True)
     updated      = models.DateTimeField(auto_now=True, auto_now_add=False)
     # slug = models.SlugField(unique=True)
     # image = models.ImageField(upload_to=upload_location,
     #                         null=True, 
     #                         blank=True,)
     # draft = models.BooleanField(default=False)
+
+
+    def save(self, *args, **kwargs):
+    	if not self.id:
+    		created_at = timezone.now()
+    	updated = timezone.now()
+    	return super(Post, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
